7d0c0f92d69f6ca57ff26f923698c0b439645c08
Zhenqiang Ding <zhenqiang.ding@gmail.com>
2019-11-25 09:08:03 +0800

    Eliminate warnings and typos

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -6,21 +6,23 @@ import unittest
 
 from calc import addition
 
+
 class AdditionTest(unittest.TestCase):
-    '''
+    """
     Test addition module
-    '''
-    
+    """
+
     def test_questions(self):
-        '''
+        """
         Test questions generator function
-        '''
+        """
         limit = 100
-        asker = addition.questions(limit)
+        questioner = addition.questions(limit)
         for _ in range(5):
-            question, solution = next(asker)
+            question, solution = next(questioner)
             left, right, *_ = re.split(r'[+=]', question)
             self.assertEqual(int(left.strip()) + int(right.strip()), solution)
+
 
 if __name__ == '__main__':
     unittest.main()
